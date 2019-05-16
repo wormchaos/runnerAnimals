f22dee5c82a791111672d0b84644788b9718eb8d
zhangwenqiang <zhangwenqiang@mobanker.com>
2019-05-16 10:27:50 +0800

    add Utils

---
+++ src/test/java/com/wormchaos/RunnerAnimalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wormchaos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunnerAnimalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
